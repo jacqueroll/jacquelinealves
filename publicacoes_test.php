3da05cb4cff0daa51be99e4b1c8931fb49061717
Jacqueline Alves <jacqueroll@gmail.com>
2026-09-18 23:13:17 -0300

    Delete publicacoes_test.php

--- publicacoes_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Arquivo de diagnóstico - apagar depois de testar
-echo "<h1>PHP está funcionando!</h1>";
-echo "<p>Versão: " . phpversion() . "</p>";
-echo "<p>Pasta: " . __DIR__ . "</p>";
-$posts_dir = __DIR__ . '/api/posts/';
-echo "<p>Pasta posts existe: " . (is_dir($posts_dir) ? 'SIM' : 'NÃO') . "</p>";
